ab39f3bc3c53abb9e770a7b67efe652bc02fd352
DannieJWright <Danniejw@gmail.com>
2019-07-24 21:44:46 -0500

    adds backend maven project

---
+++ src/main/java/com/revature/WRAP/WrapApplication.java
@@ -0,0 +1,13 @@
+package com.revature.WRAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrapApplication.class, args);
+	}
+
+}
