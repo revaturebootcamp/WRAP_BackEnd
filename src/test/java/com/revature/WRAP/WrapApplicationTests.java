7daaddc9ca622e530850f25d7c159da045ebc2ba
ben76013 <bigben37037@gmail.com>
2019-08-01 19:05:34 -0500

    Committing latest code

--- src/test/java/com/revature/WRAP/WrapApplicationTests.java
+++ src/test/java/com/revature/WRAP/WrapApplicationTests.java
@@ -1,16 +1,21 @@
 package com.revature.WRAP;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.revature.models.Ingredient;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WrapApplicationTests {
-
+public class WrapApplicationTests 
+{
 	@Test
-	public void contextLoads() {
+	public void contextLoads() 
+	{
+		
 	}
-
 }
